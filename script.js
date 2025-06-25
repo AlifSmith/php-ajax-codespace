e44ddd92f8e84936073580ffa243539859d9c321
AlifSmith <alifsmith30@gmail.com>
2025-06-25 06:36:29 +0000

    Refactor quote fetching functionality and add random quotes endpoint

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-document.getElementById("fetchData").addEventListener("click", () => {
-  fetch("server.php")
+let fontIndex = 0;
+
+function fetchAndShowQuote() {
+  const fonts = ["Barriecito", "Sora", "Matangi", "Roboto", "Lobster", "Montserrat", "Indie Flower"];
+  fetch("randomQuotes.php")
     .then((res) => res.text())
     .then((data) => {
-      document.getElementById("result").innerHTML = data;
+      const quoteContainer = document.getElementById("quote-container");
+      quoteContainer.innerText = data;
+      quoteContainer.style.display = "block";
+      quoteContainer.style.fontFamily = `"${fonts[fontIndex]}", sans-serif`;
+      quoteContainer.classList.add("fade-in");
+      setTimeout(() => quoteContainer.classList.remove("fade-in"), 1000);
+      fontIndex = (fontIndex + 1) % fonts.length;
     })
     .catch((err) => {
       console.error("AJAX Error:", err);
+      alert("Ajax Error");
     });
-});
+}
+
+document.getElementById("fetchData").addEventListener("click", fetchAndShowQuote);
+
